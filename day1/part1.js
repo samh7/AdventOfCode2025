3c83034e36493eb72606fd10a13f2d6cc3777d2f
samh7 <samh74490@gmail.com>
2025-12-01 23:45:25 +0300

    tweaks

--- day1/part1.js
+++ day1/part1.js
@@ -3,7 +3,7 @@
 
 import text from "./str.js"
 
-function getSignedNumbers(str) {
+export function getSignedNumbers(str) {
     return str.map((s) => {
         // get first letter and the rest of the list
         const [letter, ...numbers] = s;
@@ -17,12 +17,11 @@ function getSignedNumbers(str) {
 }
 
 // get remainder as sum when value is > 100  or < 0
-function calculateSum(acc, curr) {
+export function calculateSum(acc, curr) {
     let sum = acc + curr
-    return (sum < 0) ? sum %= 100 : (sum > 99) ? sum %= 100 : sum
+    return (sum < 0 || sum > 99) ? sum %= 100 : sum
 }
 
-let listForSums = []
 
 const INITIAL_VALUE = 50
 
@@ -30,12 +29,16 @@ const splitTextList = text.trim().split('\n')
 
 const signedNumbers = getSignedNumbers(splitTextList)
 
+let  password = 0;
+
 signedNumbers.reduce((currentSum, currNum) => {
-    const sum = calculateSum(currentSum, currNum)
-    listForSums.push(sum)
+    const sum =  calculateSum(currentSum, currNum) 
+    password = sum === 0 ? password + 1 : password
     return sum
 }, INITIAL_VALUE)
 
-const password = listForSums.filter((num) => num === 0).length
 
-console.log(password)
+
+// if (require.main === module){
+    console.log(password)
+// }
